a22d61c23e806ebe5e3e7aa25b649fc7c0f137ca
snelusha <hello@snelusha.dev>
2025-12-03 00:26:36 +0530

    feat: add installation-info module to detect package manager

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import fs from "fs";
 
+import { getInstallationInfo } from "~/installation-info.ts";
+
 const cliPath = process.argv[1];
 const realPath = fs.realpathSync(cliPath!).replace(/\\/g, "/");
 
 console.log(cliPath);
 console.log(realPath);
+
+const installationInfo = await getInstallationInfo();
+console.log(installationInfo);
